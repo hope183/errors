44079338108e8e57663971141a98e0b8a17e292d
97111309 <97111309@qq.com>
2019-08-20 11:43:04 +0800

    修改code int->string

--- error.go
+++ error.go
@@ -4,33 +4,32 @@ import (
 	"encoding/json"
 	"fmt"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
 var digitsRegexp = regexp.MustCompile(`:\-?(0|[1-9]\d{0,}).+?:\\?"([\S\s]+)\\?"`)
 
 type customError struct {
-	Code    int    `json:"code"`
+	Code    string `json:"code"`
 	Message string `json:"message"`
 }
 
 // New 初始化一个错误体
-func New(code int, msg string) error {
+func New(code, msg string) error {
 	return &customError{code, msg}
 }
 
 func (e *customError) Error() string {
-	return fmt.Sprintf(`{"code":%d,"message":"%s"}`, e.Code, e.Message)
+	return fmt.Sprintf(`{"code":%s,"message":"%s"}`, e.Code, e.Message)
 }
 
 // Code 获取错误码
-func Code(err error) int {
+func Code(err error) string {
 	cus, ok := err.(*customError)
 	if ok {
 		return cus.Code
 	}
-	return -99999
+	return "-99999"
 }
 
 // Message 获取错误信息
@@ -43,10 +42,10 @@ func Message(err error) string {
 }
 
 // CodeWithString 获取自定义错误的错误码
-func CodeWithString(err string) int {
+func CodeWithString(err string) string {
 	e := &customError{}
 	if json.Unmarshal([]byte(err), e) != nil {
-		return -99999
+		return `-99999`
 	}
 	return e.Code
 }
@@ -64,13 +63,12 @@ func parseError(msg string) (ok bool, err error) {
 	msgs := digitsRegexp.FindStringSubmatch(msg)
 	if len(msgs) < 3 {
 		return false, &customError{
-			Code:    -1,
+			Code:    `-1`,
 			Message: msg,
 		}
 	}
-	code, _ := strconv.ParseInt(msgs[1], 10, 64)
 	return true, &customError{
-		Code:    int(code),
+		Code:    msgs[1],
 		Message: strings.TrimSpace(msgs[2]),
 	}
 }
